ae0c5fbf8f777ed58e6fb50a1cf0aa1de7816fe0
januarabiyyu <januarabiyyusisnurfauzan@gmail.com>
2018-01-22 00:01:09 +0700

    Sprint 1

---
+++ application/controllers/admin.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+
+	function __construct(){
+		parent::__construct();
+		$this->load->model('m_data');
+		$this->load->library('encryption');
+		if ($this->session->userdata('status')!='login') {
+			redirect(base_url('login'));
+		}
+	 }
+	public function index()
+	{
+		$data['tb_user']= $this->m_data->tampil_data()->result();
+		$this->load->view('v_admin',$data);
+	}
+}
---
+++ application/controllers/crud.php
@@ -0,0 +1,76 @@
+<?php 
+ 
+class Crud extends CI_Controller{
+ 
+	function __construct(){
+		parent::__construct();		
+		$this->load->model('m_data');
+		$this->load->helper('url');
+		$this->load->library('encryption');
+ 
+	}
+ 
+	function index(){
+		$data['tb_user'] = $this->m_data->tampil_data()->result();
+		$this->load->view('v_admin',$data);
+	}
+ 
+	function tambah(){
+		$this->load->view('v_admin_data_tambah_user');
+	}
+ 
+	function tambah_aksi(){
+	
+		$fullname = $this->input->post('fullname');
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+		$level = $this->input->post('level');
+		$data = array(
+			'fullname' => $fullname,
+			'username' => $username,
+			'password' => base64_encode($password),
+			'level' => $level
+		);
+ 
+		$this->m_data->input_data($data,'tb_user');
+		redirect('admin/');
+	}
+
+	function edit($id){
+	$where = array('id' => $id);
+	$data['tb_user'] = $this->m_data->edit_data($where,'tb_user')->result();
+	$this->load->view('v_admin_data_edit_user',$data);
+	}
+ 
+	function hapus($id){
+		$where = array('id' => $id);
+		$this->m_data->hapus_data($where,'tb_user');
+		redirect('admin/');
+	}
+
+	function update(){
+	$id = $this->input->post('id');
+	$fullname = $this->input->post('fullname');
+	$username = $this->input->post('username');
+	$password = $this->input->post('password');
+	$level = $this->input->post('level');
+	$data = array(
+		'fullname' => $fullname,
+		'username' => $username,
+		'password' => base64_encode($password),
+		'level' => $level
+	);
+ 
+	$where = array(
+		'id' => $id
+	);
+
+	$this->m_data->update_data($where,$data,'tb_user');
+	redirect('admin/');
+	
+	// $query = $this->db->query('UPDATE `tb_user` SET `username` = "'.$username.'", `password` = "'.$password.'", `fullname` = "'.$fullname.'", `level` = "'.$level.'" WHERE `tb_user`.`id` = "'.$id.'"');
+	// 	if ($query) {
+	// 		echo redirect('admin/');
+	// 	}
+	}
+}
